63430942650faac85b3069027d6c7de32b0be3ed
FranciscoPasini <164237310+FranciscoPasini@users.noreply.github.com>
2026-06-22 16:24:12 -0300

    Audio Clip a la tablet

--- Assets/Scripts/Tablet/TabletUI.cs
+++ Assets/Scripts/Tablet/TabletUI.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class TabletUI : MonoBehaviour
 {
+    [Header("Pantallas")]
     [SerializeField] private GameObject powerOff;
     [SerializeField] private GameObject powerOn;
     [SerializeField] private GameObject unlockScreen;
     [SerializeField] private GameObject menuScreen;
     [SerializeField] private GameObject workSpaceScreen;
 
+    [Header("Componentes")]
     [SerializeField] private Animator animator;
+    [SerializeField] private AudioSource clickAudioSource;
 
     private Action onPowerOnAction;
 
@@ -21,6 +24,11 @@ public class TabletUI : MonoBehaviour
 
     public void OpenPanel(GameObject panel)
     {
+        if (clickAudioSource != null)
+        {
+            clickAudioSource.Play();
+        }
+
         powerOff.SetActive(false);
         powerOn.SetActive(false);
         unlockScreen.SetActive(false);
@@ -44,7 +52,6 @@ public class TabletUI : MonoBehaviour
     private IEnumerator PlayFadeInAndTransition()
     {
         animator.Play("FadeIn");
-
         yield return null;
 
         while (animator.GetCurrentAnimatorStateInfo(0).IsName("FadeIn") &&
@@ -52,6 +59,7 @@ public class TabletUI : MonoBehaviour
         {
             yield return null;
         }
+
         OpenPanel(unlockScreen);
     }
 }
